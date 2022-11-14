ea7576a6d47d6abc3fa85e3a2f56b7f6ef5ddaa1
nitza <nitzan.la4@gmail.com>
2022-11-14 11:45:00 +0200

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	src/main/java/Repository/Repository.java
    #	target/classes/org/example/User.class

---
+++ src/main/java/repository/Add.java
@@ -0,0 +1,68 @@
+//package Repository;
+//
+//import java.lang.reflect.Field;
+//import java.sql.PreparedStatement;
+//import java.sql.SQLException;
+//import java.util.List;
+//
+//public class Add<T> extends RepositoryFacade<T> {
+//
+//    public Add(Class<T> clz) {
+//        super(clz);
+//    }
+//    //---add---
+////    public void addSingleItem(T item) throws SQLException, ClassNotFoundException {
+////        openConnectionToDB();
+////        add(item);
+////        closeConnectionToDB();
+//    }
+//    public void addMultipleItem(List<T> items) throws SQLException, ClassNotFoundException {
+//        openConnectionToDB();
+//        for(T item:items){
+//            add(item);
+//        }
+//        closeConnectionToDB();
+//    }
+//
+//    public void add(T item) throws SQLException {
+//        //example: statement.executeUpdate("INSERT INTO Customers " + "VALUES (1001, 'Simpson', 'Mr.', 'Springfield', 2001)");
+//        String SQL_Statement = String.format("INSERT INTO %s", this.clz.getSimpleName().toLowerCase());
+//        SQL_Statement += "VALUES(";
+//
+//        Field[] declaredFields = clz.getDeclaredFields();
+//        for (Field field : declaredFields) {
+//            field.setAccessible(true);
+//            SQL_Statement += field.getName() + ",";
+//        }
+//        SQL_Statement=SQL_Statement.substring(0, SQL_Statement.length() - 1); //remove last ,
+//        SQL_Statement += ")";
+//        statement.executeUpdate(SQL_Statement);
+//        closeConnectionToDB();
+//    }
+//
+//    //---update---
+//
+//    public void update(T item ) throws SQLException {
+//        //TODO- implement again
+//        String sql =
+//                "UPDATE GLOBALSETTINGS " +
+//                        "  SET settingValue = ? " +
+//                        "WHERE settingName = ?";
+//        PreparedStatement pstmt = con.prepareStatement(sql);
+//
+//        Field[] declaredFields = clz.getDeclaredFields();
+//
+//        int i=0;
+//        for (Field field : declaredFields) {
+//            pstmt.setString(++i, field.getName());
+//        }
+//        pstmt.executeUpdate();
+//    }
+//
+//
+//
+//
+//
+//
+//}
+//
--- src/main/java/repository/CreateTable.java
+++ src/main/java/repository/CreateTable.java
@@ -1,4 +1,4 @@
-package Repository;
+package repository;
 
 public class CreateTable {
 }
