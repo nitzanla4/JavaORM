26cfc16ce8193a3bfa07a83e735bfad7e0d7c8fb
hadi6645 <hadi6645@gmail.com>
2022-11-15 14:17:12 +0200

    update by property

--- src/main/java/repository/queries/Update.java
+++ src/main/java/repository/queries/Update.java
@@ -3,6 +3,8 @@ package repository.queries;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.List;
 
 public class Update<T> extends Repository<T>{
@@ -12,27 +14,35 @@ public class Update<T> extends Repository<T>{
     private static Logger logger = LogManager.getLogger(Update.class.getName());
 
 
-    public <T,K> void updateOneByProperty (K property , String colName, int id)  {
+    public <T,K> void updateOneByProperty (String colName, T property, String filterBy, K filterValue)  {
         try {
             openConnectionToDB();
-            updateByProperty(property,colName, id);
+            updateByProperty(colName, property, filterBy, filterValue);
             closeConnectionToDB();
         } catch (ClassNotFoundException e) {
             logger.error("class not found!");
             e.printStackTrace();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
     }
 
-    public <T,K> List<T> updateByProperty (K property , String colName, int id) throws ClassNotFoundException {
+    public <T,K> void updateByProperty (String colName, T property, String filterBy, K filterValue) throws ClassNotFoundException, SQLException {
         String str= "UPDATE " + this.clz.getSimpleName().toLowerCase() +
                 " SET " + colName + "=";
         if(property.getClass() == String.class)  {
             str += "'" + property + "'";
         }
         else str += property;
-        str += " WHERE " + "id=" + id;
+        str += " WHERE " + filterBy + "=";
+        if(filterValue.getClass() == String.class)  {
+            str += "'" + filterValue + "'";
+        }
+        else str += filterValue;
 
         System.out.println(str);
-        return executeQuery(str);
+        statement= (Statement) con.createStatement();
+        int rows = statement.executeUpdate(str);
+        System.out.println("Rows impacted : " + rows );
     }
 }
